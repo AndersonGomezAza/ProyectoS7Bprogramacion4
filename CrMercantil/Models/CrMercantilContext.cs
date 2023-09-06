19c065c9c0884dfb47b444a57cd2e783f9449f5a
AndersonGomezAza <argomeza@itc.edu.co>
2023-09-05 20:52:31 -0500

    Fix Errors In BackEnd

--- CrMercantil/Models/CrMercantilContext.cs
+++ CrMercantil/Models/CrMercantilContext.cs
@@ -33,7 +33,7 @@ public partial class CrMercantilContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-N1LLJQJ\\SQLEXPRESS; Database=cr_mercantil; Trusted_Connection=True; TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-HNG3HQ1\\SQLEXPRESS; Database=cr_mercantil_bbdd; Trusted_Connection=True; TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
